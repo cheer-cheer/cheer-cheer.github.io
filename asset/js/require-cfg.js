353fd58f1b0e9eeba06a79218a4684b224feb8db
cheer <riemann_hypothesis@163.com>
2014-07-17 21:24:37 +0800

    test

--- asset/js/require-cfg.js
+++ asset/js/require-cfg.js
@@ -1,7 +1,7 @@
-require.config({
-  baseUrl: 'asset/js',
-  paths: {
-    jquery: 'jquery-1.9.0'
-  }
-
-});
+// require.config({
+//   baseUrl: 'asset/js',
+//   paths: {
+//     jquery: 'http://libs.baidu.com/jquery/2.0.0/jquery.min.js'
+//   }
+// });
+alert('requiring...')
